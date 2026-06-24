c2b2570b31966f34c0f6166b2c2969e542054926
Bagdat <68787724+BagiYak@users.noreply.github.com>
2026-06-24 14:12:12 +0500

    Notes - Update submission-0.py
    # https://neetcode.io/problems/duplicate-integer/notes
    ## Solved in 10 min.
    
    ##First idea: Brute Force - check each num in the list using for loop twice:
    # n = len(nums)
    # for i in range(n):                # Outer loop goes from 0 to the end
    #    for j in range(i + 1, n):     # Inner loop goes from next num to the end and checks nums
    #        if nums[i] == nums[j]:
    #            return True
    # return False
    # Understand that it's bad solution due to TimeComplexity `\(O(n^2)\)`
    
    ## Second idea: set() - TimeComplexity O(n) and MemoryComplexity O(n)

--- Data Structures & Algorithms/duplicate-integer/submission-0.py
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -1,4 +1,17 @@
 # https://neetcode.io/problems/duplicate-integer/history
+# https://neetcode.io/problems/duplicate-integer/notes
+## Solved in 10 min.
+
+##First idea: Brute Force - check each num in the list using for loop twice:
+# n = len(nums)
+# for i in range(n):                # Outer loop goes from 0 to the end
+#    for j in range(i + 1, n):     # Inner loop goes from next num to the end and checks nums
+#        if nums[i] == nums[j]:
+#            return True
+# return False
+# Understand that it's bad solution due to TimeComplexity `\(O(n^2)\)`
+
+## Second idea: set() - TimeComplexity O(n) and MemoryComplexity O(n)
 
 from typing import List
 
